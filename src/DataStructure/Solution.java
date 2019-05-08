b250b40bf552067cafe281c66ca9c94b93f86930
ssyup <ssyup@LAPTOP-C5FJE2OK>
2019-05-08 16:47:21 +0900

    Tree

---
+++ src/DataStructure/Solution.java
@@ -0,0 +1,33 @@
+package DataStructure;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; ++test_case) {
+			int node = sc.nextInt();
+			int edge = sc.nextInt();
+
+			Tree tree = new Tree(node);
+
+			for (int i = 0; i < edge; i++) {
+				int parent = sc.nextInt();
+				int child = sc.nextInt();
+				tree.addChild(parent, child);
+			}
+			int root = tree.getRoot();
+			System.out.printf("#%d ", test_case);
+			tree.preOrder(root);
+			System.out.printf("\n");
+		}
+
+		sc.close();
+
+	}
+
+}
