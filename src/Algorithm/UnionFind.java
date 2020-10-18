667a73ac8fc12d712309d276bc8ba8448377805f
ssyup4259 <ssyup8717@gmail.com>
2020-10-18 21:27:15 +0900

    study algo

--- src/Algorithm/UnionFind.java
+++ src/Algorithm/UnionFind.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 public class UnionFind {
 	static int n, m, p[];
-
+	
+	// 현재 원소의 부모를 찾는다.
 	static int find(int node) {
 		if (p[node] != node) {
 			p[node] = find(p[node]);
 		}
 		return p[node];
 	}
-
+	
+	// 연결되어 있지 않을때 연결 시킨다.
 	static void union(int a, int b) {
 		a = find(a);
 		b = find(b);
@@ -30,7 +32,8 @@ public class UnionFind {
 		n = sc.nextInt();
 		m = sc.nextInt();
 		p = new int[n + 1];
-
+		
+		// 초기에 각 원소들의 부모는 자기 자신이다.
 		for (int i = 1; i <= n; i++) {
 			p[i] = i;
 		}
